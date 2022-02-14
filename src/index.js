8ba7feb1d13690e2eabd47bd9ef2cbf5c5759d03
7SilviaT <eagles8485@gmail.com>
2022-02-14 13:11:40 -0500

    Primeras funciones en JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
+//evento capturar numero de tarjeta
+let btnSend = document.getElementById("btn");
+btnSend.addEventListener("click", () => {
+  getNumber();
+});
+
+//funcion para vaidar digitos del numero de tarjeta
+function getNumber() {
+let cardNumber1 = document.getElementById ("cardNumber").value;
+if (cardNumber1.length <10 ){
+  alert("Ingrese número de tarjeta válido. Debe contener mínimo 10 dígitos ")
+} else if (cardNumber1.length === 16)
+alert ("Gracias por apoyar el arte peruano")
+else {
+  validator.isValid(cardNumber1)
+}
+}
+
+
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,9 @@
 const validator = {
-  // ...
+isValid: function (cardNumber1) {
+    console.log(cardNumber1);
+    return cardNumber1 },
+maskify: function (cardNumber1) {
+    return cardNumber1}
 };
 
 export default validator;
