aabec02c93747fe623f9744f13dddd1a53b81d6f
7SilviaT <eagles8485@gmail.com>
2022-03-03 15:19:31 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,34 @@
+import validator from "./validator.js";
+// mostrar y ocultar paginas
+const btnValid1= document.getElementById("btn1");
+btnValid1.addEventListener("click",()=>{
+const pageOne=document.getElementById("pageOne");
+pageOne.style.display="none";
+const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="";
+});
+const btnValid2= document.getElementById("btn3");
+btnValid2.addEventListener("click",()=>{
+const pageOne=document.getElementById("pageOne");
+pageOne.style.display="";
+const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="none";
+});
+
+
+//evento capturar numero de tarjeta y pasar por validaciones y mensajes
+const btnValidate = document.getElementById("btn");
+btnValidate.addEventListener("click",()=>{
+  //e.preventDefault()
+  let cardNumber= document.getElementById("cardNumber").value;
+  let maskedNumber = validator.maskify(cardNumber);
+  let validCardUser=validator.isValid(cardNumber);
+
+  if (cardNumber===""){
+    document.getElementById("formInputError").innerText = " ❌Por favor ingrese su número de tarjeta";
+  }else if (validCardUser===true){
+    document.getElementById("formInputError").innerText = "✔️Su tarjeta " + maskedNumber + " es válida.Gracias por su compra";
+  }else{
+    document.getElementById("formInputError").innerText = "Su tarjeta " + maskedNumber + " es inválida.Intente nuevamente";
+  }
+} )
---
+++ validator.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+
+const validator = {
+isValid: function (cardNumber) {
+//convertimos a array
+let arrayFirst=Array.from(cardNumber);
+console.log(arrayFirst);
+//revertir array
+const reversedArray= arrayFirst.reverse();
+console.log (reversedArray);
+//string a numeros
+let arrayTwo =[];
+for (let i=0;i<arrayFirst.length;i++){
+arrayTwo.push(parseInt(reversedArray[i]));
+}
+console.log(arrayTwo);
+// recorrer el array identificando num en posicion impar(PI) para JS
+let finalResult = 0;
+for(let i= 0;i<arrayTwo.length;i++){
+    if(i % 2!==0){
+// multiplicar*2 los digitos de PI y sumar los digitos mayores a 10
+        arrayTwo[i] = arrayTwo[i]*2;
+        if (arrayTwo[i] >=10){
+            arrayTwo[i]=arrayTwo[i]-9;
+        }
+    }
+// sumar todos los numeros del array
+    finalResult =finalResult+ arrayTwo[i];
+}
+// suma total debe ser multiplo de 10 para ser tarjeta valida
+if (finalResult % 10===0) {
+console.log(true);
+    return true;
+} else {
+console.log(false);
+    return false;
+}
+},
+maskify:function (cardNumber){
+    let maskedNum = Array.from(cardNumber);
+    let stringNum;
+    for(let i = 0; i < maskedNum.length-4; i++){
+    maskedNum[i]= '#';
+    }
+    stringNum= maskedNum.join("");
+    console.log(stringNum);
+    return stringNum;
+}
+}
+export default validator;
